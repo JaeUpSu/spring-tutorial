7b59279d867d3b784f112b94c2b3419e754587d5
khs <hyeongs2323@gmail.com>
2024-04-09 15:40:08 +0900

    feat : AOP Settings (hello.hellospring package)

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceApp;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
@@ -16,4 +17,9 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceApp timeTraceApp() {
+//        return new TimeTraceApp();
+//    }
 }
